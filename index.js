8ceff5e26c22e4a98ccf964a9c6371da934b45e3
Eric Storey <estorey@gmail.com>
2019-06-18 18:53:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
   return newDriver
 }
 
-function 
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]=value
+  return driver
+}
